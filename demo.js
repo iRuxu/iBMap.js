d9564e8d768bf99e69b4c270cb19e715ce4342c0
iRuxu <rx6@qq.com>
2018-04-28 20:12:08 +0800

    v1.0

--- demo.js
+++ demo.js
@@ -3,6 +3,14 @@ jQuery(function ($) {
     iBMap.init('jKIIKKqQqspECMVMdZZGcXsGRj62sPQS', 'map')
 
     iBMap.geocode('北京市海淀区上地十街10号')
+
+    iBMap.position({
+        lng:112.81271748030389,
+        lat:45.16277754545299,
+        zoom:5
+    })
+
+    iBMap.cn()
     
     iBMap.marker({
         lng : 116.307852,   //地理纬度
@@ -11,44 +19,44 @@ jQuery(function ($) {
         content : '北京市海淀区上地十街10号', //显示的内容
         icon : {
             url:'http://lbsyun.baidu.com/jsdemo/img/fox.gif', //自定义图标
-            width:39,
-            height:25
-        }
-    },true)
-
-    iBMap.position({
-        lng:112.81271748030389,
-        lat:45.16277754545299,
-        zoom:5
+            width:360,
+            height:170
+        },
+        status:true,
+        zoom:16
     })
 
     iBMap.markers({
-        center: {lng:116.417854, lat:39.921988},
-        zoom: 16,
-        arr: [
+        center: { lng: 116.417854, lat: 39.921988 },
+        data: [
             {
                 lng: 116.417854,
                 lat: 39.921988,
-                title:'地址1',
+                title: '地址1',
                 content: "北京市东城区王府井大街88号乐天银泰百货八层",
-                icon : 'http://lbsyun.baidu.com/jsdemo/img/fox.gif'
+                icon: {
+                    url:'http://lbsyun.baidu.com/jsdemo/img/fox.gif',
+                    width:300,
+                    height:170
+                },
+                status:true
             },
             {
                 lng: 116.406605,
                 lat: 39.921585,
-                title:'地址2',
-                content: "地址：北京市东城区东华门大街",
+                title: '地址2',
+                content: "地址：北京市东城区东华门大街"
             },
             {
                 lng: 116.412222,
                 lat: 39.912345,
-                title:'地址3',
+                title: '地址3',
                 content: "地址：北京市东城区正义路甲5号",
             }
         ]
     })
 
-    iBMap.URI.marker('map',{
+    iBMap.URI.marker('mapx',{
         lat:39.916979519873,
         lng:116.41004950566
     })
--- iBMap.js
+++ iBMap.js
@@ -7,25 +7,71 @@
 
 var iBMap = {
 
-    version: '0.2',
-    github : 'git@github.com:iRuxu/iBMap.git',
-    BMap: {},
-    ak: '',
-    c_lat:0,
-    c_lng:0,
-    zoom:16,
+    version: '1.0',
+    github: 'git@github.com:iRuxu/iBMap.git',
 
-    //以下方法需在页面加载api文件及SearchInfoWindow插件相关文件
+    //缓存创建的百度地图实例对象
+    BMap: {},
+
+    //存储ak用于Web API
+    ak: '',
+
+    //存储当前的地理信息
+    _lat: 0,
+    _lng: 0,
+    _zoom: 5,
+    _title: '',
+    _content: '',
+    _status: false,  //默认信息窗关闭
+    _width: 0,
+    _height: 0,
+    _icon: {},
+
+    //iBMap.geo 默认的地理对象
+    geo: {
+        lat: 35.16277754545299,
+        lng: 112.81271748030389,
+        zoom: 16,
+        title: '我的位置',
+        content: '目的地',
+        status: false,
+        width: 290,
+        height: '',
+        icon: {
+            url: '',
+            width: 0,
+            height: 0
+        },
+    },
+
+    //主方法需在页面加载api文件及SearchInfoWindow插件相关文件
+    /**
+     * init 初始化
+     * @desc 创建百度地图实例对象，控件控制
+     * @param {string} ak 百度ak密钥
+     * @param {string} selectorID 选择器id
+     * @returns {object} 返回iBMap对象
+     */
     init: function (ak, selectorID) {
         iBMap.ak = ak
         iBMap.BMap = new BMap.Map(selectorID);
+
+        //控件开启
         iBMap.BMap.addControl(new BMap.NavigationControl());
         iBMap.BMap.enableScrollWheelZoom();
+
+        return iBMap
     },
 
+    /**
+     * geocode 正地理编码
+     * @desc 将字符串转为iBMap地理信息geo对象
+     * @param {string} queryAddress 查询的字符串
+     * @param {function} callback 查询接口获取编码后执行的回调函数，传入一个iBMap地理信息geo对象
+     */
     geocode: function (queryAddress, callback) {
         if (!queryAddress) {
-            console.error('[iBMap]正逆地理编码失败，请输入有效的字符串地址')
+            console.error('[iBMap]正地理编码失败，请输入有效的字符串地址')
         }
         $.ajax({
             url: 'http://api.map.baidu.com/geocoder/v2/',
@@ -49,42 +95,79 @@ var iBMap = {
         })
     },
 
-    position : function (geo){
-        iBMap.c_lng = geo.lng
-        iBMap.c_lat = geo.lat
-        iBMap.zoom = geo.zoom
-        var point = new BMap.Point(geo.lng,geo.lat);
+    /**
+     * position 定位
+     * @desc 定位中心坐标与放缩倍数
+     * @param {object} geo iBMap地理信息geo对象
+     * @returns 返回iBMap对象
+     */
+    position: function (geo) {
+        iBMap._lng = geo.lng || iBMap.geo.lng
+        iBMap._lat = geo.lat || iBMap.geo.lat
+        iBMap._zoom = geo.zoom || iBMap.geo.zoom
+        var point = new BMap.Point(geo.lng, geo.lat);
         iBMap.BMap.centerAndZoom(point, geo.zoom);
+
+        return iBMap
     },
 
-    marker: function (geo,status,callback) {
-        //自定义设置
-        var lat = geo.lat
-        var lng = geo.lng
-        var zoom = geo.zoom || 16
-        var title = geo.title || ''
-        var content = geo.content || ''   //可以为自定义html
-        var width = geo.width || 290
-        var height = geo.height || ''
+    /**
+     * cn 中国
+     * @desc 快捷定位到中国概貌
+     * @returns 返回iBMap对象
+     */
+    cn: function () {
+        var point = new BMap.Point(iBMap.geo.lng, iBMap.geo.lat);
+        iBMap.BMap.centerAndZoom(point,5);
 
-        //创建坐标点
-        var point = new BMap.Point(lng, lat);
-        if(!iBMap.c_lat && !iBMap.c_lng){   //如果已经设置过中心坐标就不再设置了，用于数据回调中手动批量创建
-            iBMap.BMap.centerAndZoom(point,zoom);
+        return iBMap
+    },
+
+    fn : {
+        openInfo : function(){
+            
+        }  
+    },
+
+
+    /**
+     * marker 单标记
+     * @desc 添加单个标记
+     * @param {object} geo iBMap地理信息geo对象
+     * @param {function} callback 点击标记后执行的回调函数，传入iBMap地理信息geo对象
+     * @returns {object} 返回iBMap对象
+     */
+    marker: function (geo, callback) {
+
+        if (!geo.lng || !geo.lat) {
+            console.error('[iBMap]添加标记失败,传入的地理信息对象有误')
         }
 
-        //添加标注
+        //重定义当前iBMap属性
+        iBMap._lng = geo.lng
+        iBMap._lat = geo.lat
+        iBMap._zoom = geo.zoom || iBMap.geo.zoom
+        iBMap._title = geo.title || ''
+        iBMap._content = geo.content || ''   //可以为自定义html
+        iBMap._width = geo.width || iBMap.geo.width
+        iBMap._height = geo.height || iBMap.geo.height
+
+        //创建坐标点
+        var point = new BMap.Point(geo.lng, geo.lat);
+        iBMap.BMap.centerAndZoom(point, iBMap._zoom);
+
+        //添加标记与设置图标
         if (geo.icon) {
-            var myIcon = new BMap.Icon(geo.icon.url, new BMap.Size(geo.icon.width,geo.icon.height));
+            var myIcon = new BMap.Icon(geo.icon.url, new BMap.Size(geo.icon.width, geo.icon.height));
             var marker = new BMap.Marker(point, { icon: myIcon });
         } else {
             var marker = new BMap.Marker(point);
         }
         iBMap.BMap.addOverlay(marker);
-        addClickHandler()
+        addClickHandler(geo)
 
-        //信息窗事件
-        function addClickHandler(){
+        //绑定标记点击事件
+        function addClickHandler(geo) {
             marker.addEventListener("click", function (e) {
                 openInfo()
                 !!callback && callback(geo)
@@ -92,13 +175,12 @@ var iBMap = {
             );
         }
 
-        //创建指定信息窗
-        function openInfo(){
-            //创建检索信息窗口对象
-            var searchInfoWindow = new BMapLib.SearchInfoWindow(iBMap.BMap, content, {
-                title: title,      //标题
-                width: width,      //宽度
-                height: height,     //高度
+        //创建检索信息窗口对象
+        function openInfo() {
+            var searchInfoWindow = new BMapLib.SearchInfoWindow(iBMap.BMap, iBMap._content, {
+                title: iBMap._title,      //标题
+                width: iBMap._width,      //宽度
+                height: iBMap._height,     //高度
                 panel: "panel",        //检索结果面板
                 enableAutoPan: true,    //自动平移
                 searchTypes: [
@@ -107,54 +189,75 @@ var iBMap = {
                     BMAPLIB_TAB_FROM_HERE//从这里出发
                 ]
             });
-            searchInfoWindow.open(point, zoom);
+            searchInfoWindow.open(point, iBMap._zoom);
         }
 
         //是否默认显示信息窗
-        !!status && openInfo()
+        !!geo.status && openInfo()
+
+        return iBMap
     },
 
-    markers: function (geo,callback) {
-        //自定义设置
-        var c_lng = geo.center.lng || 39.921988
-        var c_lat = geo.center.lat || 116.417854
-        var zoom = geo.zoom || 16
-        var list = geo.arr || []
-        var len = list.length
-        var width = geo.width || 290
-        var height = geo.height || ''
-
-        //设置中心坐标点
-        iBMap.BMap.centerAndZoom(new BMap.Point(c_lng, c_lat), zoom);
-
-        //批量添加标注
-        for (var i = 0; i < len; i++) {
-            if (list[i]['icon']) {
-                var myIcon = new BMap.Icon(list[i]['icon']['url'], new BMap.Size(geo.icon.width,geo.icon.height));
-                var marker = new BMap.Marker(new BMap.Point(list[i]['lng'], list[i]['lat']), { icon: myIcon });
-            } else {
-                var marker = new BMap.Marker(new BMap.Point(list[i]['lng'], list[i]['lat']));
-            }
-
-            var content = list[i]['content'];
-            var title = list[i]['title']
-            iBMap.BMap.addOverlay(marker);// 将标注添加到地图中
-            addClickHandler(content, title,list[i]);
+    /**
+     * markers 多标记
+     * @desc 批量添加多个标记
+     * @param {object} 可集中设置统一的选项，必须包含data数组，data数组中的各项属性会覆写集中设置的值
+     * @param {object} 返回iBMap对象
+     */
+    markers: function (geo, callback) {
+        if (!geo.data || !Array.isArray(geo.data)) {
+            console.error('[iBMap]传入的标记数据有误')
         }
 
-        //信息窗事件
-        function addClickHandler(content, title, geo) {
+        //重定义当前iBMap属性
+        iBMap._lng = geo.center.lng || iBMap.geo.lng
+        iBMap._lat = geo.center.lat || iBMap.geo.lat
+        iBMap._zoom = geo.zoom || iBMap.geo.zoom
+        iBMap._width = geo.width || iBMap.geo.width
+        iBMap._height = geo.height || iBMap.geo.height
+        iBMap._title = geo.title ||  iBMap.geo.title
+        iBMap._content = geo.content || iBMap.geo.content
+
+        //设置中心坐标点
+        iBMap.BMap.centerAndZoom(new BMap.Point(geo.center.lng, geo.center.lat), iBMap._zoom);
+
+        //批量添加标记
+        for (var i = 0; i < geo.data.length; i++) {
+
+            if (geo.data[i]['icon']) {
+                var myIcon = new BMap.Icon(geo.data[i]['icon']['url'], new BMap.Size(geo.data[i]['icon']['width'], geo.data[i]['icon']['height']));
+                var marker = new BMap.Marker(new BMap.Point(geo.data[i]['lng'], geo.data[i]['lat']), { icon: myIcon });
+            } else {
+                var marker = new BMap.Marker(new BMap.Point(geo.data[i]['lng'], geo.data[i]['lat']));
+            }
+
+            iBMap.BMap.addOverlay(marker);
+            addClickHandler(geo.data[i]);
+
+            //默认显示指定的一个属性的信息窗,只能设置一个
+            !!geo.data[i].status && openInfo(geo.data[i])   
+        }
+
+        //绑定标记点击事件，并添加回调
+        function addClickHandler(geo) {
             marker.addEventListener("click", function (e) {
-                openInfo(content, e,title)
+                openInfo(geo)
                 !!callback && callback(geo)
             }
             );
         }
-        //创建指定信息窗
-        function openInfo(content, e,title) {
-            var p = e.target;
-            var point = new BMap.Point(p.getPosition().lng, p.getPosition().lat);
-            var searchInfoWindow = new BMapLib.SearchInfoWindow(iBMap.BMap, content, {
+
+        //创建信息窗
+        function openInfo(geo) {
+
+            //如果批量标注有进行全局设置
+            var content = geo.content || iBMap._content
+            var title = geo.title || iBMap._title
+            var width = geo.width || iBMap._width
+            var height = geo.height || iBMap._height
+
+            var point = new BMap.Point(geo.lng, geo.lat);
+            var searchInfoWindow = new BMapLib.SearchInfoWindow(iBMap.BMap,content, {
                 title: title,
                 width: width,      //宽度
                 height: height,     //高度
@@ -166,13 +269,15 @@ var iBMap = {
                     BMAPLIB_TAB_FROM_HERE//从这里出发
                 ]
             });
-            searchInfoWindow.open(point, zoom)
+            searchInfoWindow.open(point, iBMap._zoom)
         }
+
+        return iBMap
     },
 
     //以下通过URI API调起百度地图嵌入iframe，无需AK，无需初始化
     URI: {
-        marker: function (iframeID,geo) {
+        marker: function (iframeID, geo) {
             var mapurl = 'http://api.map.baidu.com/marker?' +
                 'location=' + geo.lat + ',' + geo.lng +
                 '&title=' + geo.title +
